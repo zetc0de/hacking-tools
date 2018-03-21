ad5ecd0e86ebb63c270cd898dff6f23b20c20850
havidzc0de <havidzc0de@gmail.com>
2018-03-21 18:58:56 +0700

    rename

--- dekripsi/com/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-##################Settings#######################
-//This settings will be used in seo pourpouses
-$site_name        = ' Encrypting & Encoding Script'; //site name
-$site_description = 'Vipc0de.org Encryption over 30 encryption altghoritms, like hex, binary, base64, Whirpool,md5 ,SHA, ROT13, and so many others.';//site description (~250 characters)
-$site_keywords    = 'binary,des,hex,online converter';//a few keywords,separated by a comma
-#################################################
-require_once('functions.php');
-?>
